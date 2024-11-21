6c5ee48153b550dfa8f238ccc77bfe0e7843ec11
Alok Kumar <alokk0018@gmail.com>
2024-11-22 01:58:00 +0530

    initial commit

---
+++ src/main/java/com/weCredit/weCreditAssignment/WeCreditAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.weCredit.weCreditAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeCreditAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeCreditAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weCredit/weCreditAssignment/WeCreditAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weCredit.weCreditAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeCreditAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
